26d04b732bb2cfa01ec3eddcb1c681dd65d51494
MouadEliteHacker <mouad.benmas@gmail.com>
2026-03-18 00:29:07 +0000

    Update db.js
    Removed the hardcoded db connection

--- db.js
+++ db.js
@@ -3,13 +3,8 @@ require('dotenv').config()
 
 
 const pool = new Pool({
-  host: 'aws-1-eu-west-1.pooler.supabase.com',
-  port: 5432,
-  database: 'postgres',
-  user: 'postgres.asrjmjodvbdlbjkziuqp',
-  password: 'SupaBasePas1',
+  connectionString: process.env.DATABASE_URI,
   ssl: { rejectUnauthorized: false }
 })
-
 
 module.exports = pool
